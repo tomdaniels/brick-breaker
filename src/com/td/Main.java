532d69f5de3deeb0875a3ce6f60a78f28959c7bd
tomdaniels <thomsondaniels@hotmail.com>
2021-11-26 18:23:44 +1100

    init app window w JFrame

--- src/com/td/Main.java
+++ src/com/td/Main.java
@@ -1,8 +1,18 @@
 package com.td;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello world!");
+        JFrame obj = new JFrame();
+        GamePlay gamePLay = new GamePlay();
+
+        // initialize the window
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Brick Breaker");
+//        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
